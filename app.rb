02be73bae7f1292a0744170b5c9bc3fdd13464a7
Deon Tan <deonjtan@gmail.com>
2015-10-28 17:44:17 +0000

    Session and Redirects not persisting

--- app.rb
+++ app.rb
@@ -2,33 +2,28 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   enable :sessions  
+  set :session_secret, 'string'
 
   get '/' do
-    p params
-    p session
     erb(:index)
   end
 
   get '/play' do
+    p "GET /play"
+    p "session: #{session}"
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
-    p params
-    p session
-    p @player_1_name
-    p @player_2_name
-    p $p1n,$p2n
     erb(:play)
   end
 
   post '/names' do
+    p "POST /names"
+    p "Session before setting: #{session}"
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
-    $p1n = params[:player_1_name]
-    $p2n = params[:player_2_name]
-    p params
-    p session
-    p "/names",session[:player_1_name]
-    p $p1n,$p2n
+    p "Session after setting: #{session}"
+    # $p1n = params[:player_1_name]
+    # $p2n = params[:player_2_name]
     redirect '/play'
   end
 
--- spec/features/battle_feature_spec.rb
+++ spec/features/battle_feature_spec.rb
@@ -1,18 +1,17 @@
 require 'capybara'
 require 'selenium-webdriver'
 # require './chrome_addon'
-include Capybara::DSL
+# include Capybara::DSL
 
-Capybara.default_driver = :selenium
-
-visit 'http://localhost:4567/'
+# Capybara.default_driver = :selenium
 
 feature 'first page' do
   scenario 'entering names' do
-    # expect(page).to have_content('Hello')
-    expect(page).to have_css('#form')
-    fill_in('player_1', with: 'deon')
-    fill_in('player_2', with: 'pidgy')
+    visit '/'
+    fill_in('player_1_name', with: 'deon')
+    fill_in('player_2_name', with: 'pidgy')
     click_button('Submit')
+    expect(page).to have_content 'deon'
+    expect(page).to have_content 'pidgy'
   end
 end
