f09c00b3fda0c83c51a26f04ecd1d1cde4e037a1
Deon Tan <deonjtan@gmail.com>
2015-10-28 17:00:24 +0000

    redirect sadness

--- app.rb
+++ app.rb
@@ -1,14 +1,35 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions  
+
   get '/' do
+    p params
+    p session
     erb(:index)
   end
 
-  post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    p params
+    p session
+    p @player_1_name
+    p @player_2_name
+    p $p1n,$p2n
     erb(:play)
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    $p1n = params[:player_1_name]
+    $p2n = params[:player_2_name]
+    p params
+    p session
+    p "/names",session[:player_1_name]
+    p $p1n,$p2n
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
